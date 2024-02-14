22da55c2d653bf43b4eaf65d048492027d5212e7
Imtiaz-ahmad786 <imtiazahmad6268@gmail.com>
2024-02-14 10:23:08 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        orange: "#fd7e14",
+      },
+    },
+  },
+  plugins: [],
+};
